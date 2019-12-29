52c561feae11ed955295d9a1a74cdb3e7ccdb576
Evgeniy <silantev-zhenya@mail.ru>
2019-12-29 18:14:45 +0400

    Набросал компоненты

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import AppHeader from "./components/app-header/app-header";
+import RandomPlanet from "./components/random-planet";
+import ItemList from "./components/item-list";
+import PersonDetails from "./components/person-detail";
 
 function App() {
   return (
     <div>
       <AppHeader/>
+      <RandomPlanet/>
+      <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails/>
+          </div>
+      </div>
     </div>
   );
 }
--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,13 +2,31 @@ import React from "react";
 import './app-header.css';
 
 const AppHeader = () => {
+    const items=[
+        {
+            link:'#',
+            label:'People'
+        },
+        {
+            link:'#',
+            label:'Planets'
+        },
+        {
+            link:'#',
+            label:'Starships'
+        }
+    ];
     return (
         <div className='header d-flex'>
-            <h1>Star DB</h1>
-            <ul className='nav'>
-                <li className='nav-item'>People</li>
-                <li className='nav-item'>Planets</li>
-                <li className='nav-item'>Starships</li>
+            <h3>
+                <a href="#">Star DB</a>
+            </h3>
+            <ul className='d-flex'>
+                {items.map(item=>{
+                    return (<li>
+                        <a href={item.link}>{item.label}</a>
+                    </li>)
+                })}
             </ul>
         </div>
     );
